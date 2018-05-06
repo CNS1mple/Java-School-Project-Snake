d1449b94c49d9a1fed98d26c1001117ed3d7624b
s1mple <yang@clewyo.cc>
2018-05-06 15:32:42 +0800

    delete version 1

--- Snake/src/cn/itcast/snake/listener/SnakeListener.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.snake.listener;
-
-import cn.itcast.snake.entities.Snake;
-
-public interface SnakeListener {
-	
-	void snakeMoved(Snake snake);
-}
--- Snake/src/cn/itcast/snake/util/Global.java
+++
@@ -1,7 +0,0 @@
-package cn.itcast.snake.util;
-
-public class Global {
-	public static final int CELL_SIZE = 50;
-	public static final int WIDTH = 15;
-	public static final int HEIGHT = 15;
-}
